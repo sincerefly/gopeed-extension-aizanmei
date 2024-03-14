4a3d704b4db37b93b1ec284105d4d8f46cec92b6
sincerefly <kissbug8720@gmail.com>
2024-03-14 11:32:59 +0800

    style: format by vscode prettier extension

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-// Support URL Examples: 
+// Support URL Examples:
 // 如果 https://www.zanmei.ai/song/52195.html 标准品质试听
 // 脚步 https://www.zanmei.ai/song/11322.html 高品质 & 标准品质
 // 幸福 https://www.zanmei.ai/album/blessed.html
 
-import * as libsong from './lib/song.js';
-import * as libalbum from './lib/album.js';
-
+import * as libsong from "./lib/song.js";
+import * as libalbum from "./lib/album.js";
 
 const MediaType = {
-  SONG: 'song',
-  ALBUM: 'album'
+  SONG: "song",
+  ALBUM: "album",
 };
 
 function judgeMediaTypeFromUrl(url) {
@@ -21,28 +20,26 @@ function judgeMediaTypeFromUrl(url) {
   } else if (albumRegex.test(url)) {
     return MediaType.ALBUM;
   } else {
-    return 'unknown';
+    return "unknown";
   }
 }
 
-gopeed.events.onResolve(async function(ctx) {
+gopeed.events.onResolve(async function (ctx) {
+  const mediaType = judgeMediaTypeFromUrl(ctx.req.url);
 
-  const mediaType = judgeMediaTypeFromUrl(ctx.req.url)
-  
-  let files = []
+  let files = [];
 
+  // 媒体类型：歌曲
   if (mediaType === MediaType.SONG) {
-
-    console.log('媒体类型：歌曲');
     files = await handleSong(ctx);
 
+    // 媒体类型：专辑
   } else if (mediaType === MediaType.ALBUM) {
-
-    console.log('媒体类型：专辑');
     files = await handleAlbum(ctx);
 
+    // 媒体类型：未知
   } else {
-    console.log('未知的媒体类型');
+    throw new MessageError("未知的媒体类型");
   }
 
   ctx.res = {
@@ -53,51 +50,77 @@ gopeed.events.onResolve(async function(ctx) {
 
 // 音乐
 async function handleSong(ctx) {
-
-    const songWebUrl = ctx.req.url;
-    let file = await parserDownloadFile(songWebUrl);
-    return [
-      file
-    ]
+  const songWebUrl = ctx.req.url;
+  let file = await parserDownloadFile(songWebUrl, gopeed.settings);
+  return [file];
 }
 
-async function parserDownloadFile(songWebUrl) {
+// 专辑
+async function handleAlbum(ctx) {
+  const albumWebUrl = ctx.req.url;
+
+  const albumWebHtml = await libalbum.FetchAlbumWebHtmlAnonymous(
+    albumWebUrl,
+    gopeed.settings
+  );
+
+  const albumList = libalbum.ParserSongWebUrlList(albumWebHtml);
+
+  let files = [];
+  for (var i = 0; i < albumList.length; i++) {
+    let songItem = albumList[i];
+
+    let name = songItem["name"] + ".mp3";
+    let url = await parserDownloadUrl(songItem["path"], gopeed.settings);
+
+    let file = {
+      name: name,
+      req: {
+        url: url,
+      },
+    };
+    files.push(file);
+  }
+  return files;
+}
+
+// ------ 下载音乐 ------
+
+async function parserDownloadFile(songWebUrl, settings) {
   // Anonymous
-  let file = {}
-  if (gopeed.settings.cookie == "") {
-    file = await parserAnonymousDownloadFile(songWebUrl)
-  
-  // Authorized with Cookie
+  let file = {};
+  if (settings.cookie == "") {
+    file = await parserAnonymousDownloadFile(songWebUrl, settings);
+
+    // Authorized with Cookie
   } else {
-    file = await parserAuthorizedDownloadFile(songWebUrl)
+    file = await parserAuthorizedDownloadFile(songWebUrl, settings);
   }
   return file;
 }
 
-async function parserAnonymousDownloadFile(songWebUrl) {
+async function parserAnonymousDownloadFile(songWebUrl, settings) {
+  const palyUrl = libsong.AnonymousWebUrlToPlayUrl(songWebUrl);
 
-  const palyUrl = libsong.AnonymousWebUrlToPlayUrl(songWebUrl)
-
-  const html = await libsong.FetchSongWebHtmlAnonymous(songWebUrl, gopeed.settings);
+  const html = await libsong.FetchSongWebHtmlAnonymous(songWebUrl, settings);
 
   const songName = libsong.ParserSongName(html);
   const downName = `${songName}.mp3`;
-  
+
   return {
     name: downName,
     req: {
       url: palyUrl,
-    }
-  }
+    },
+  };
 }
 
-async function parserAuthorizedDownloadFile(songWebUrl) {
+async function parserAuthorizedDownloadFile(songWebUrl, settings) {
+  const html = await libsong.FetchSongWebHtmlAnonymous(songWebUrl, settings);
 
-  const html = await libsong.FetchSongWebHtmlAnonymous(songWebUrl, gopeed.settings);
+  const songId = libsong.ParserSongId(html);
 
-  const songId = libsong.ParserSongId(html)
-
-  const downMeta = await libsong.FetchSongDownMetaJson(songId, gopeed.settings)
+  const downMeta = await libsong.FetchSongDownMetaJson(songId, settings);
 
   // 当歌曲不提供下载时下载试听版本
   if (!downMeta["ok"]) {
@@ -106,9 +129,9 @@ async function parserAuthorizedDownloadFile(songWebUrl) {
 
   const downName = `${downMeta["song"]}.mp3`;
 
-  let downUrl = libsong.GetLowMP3UrlFromMetaJson(downMeta)
-  if (gopeed.settings.quality == "songhigh") {
-    downUrl = libsong.GetHighMP3UrlFromMetaJson(downMeta)
+  let downUrl = libsong.GetLowMP3UrlFromMetaJson(downMeta);
+  if (settings.quality == "songhigh") {
+    downUrl = libsong.GetHighMP3UrlFromMetaJson(downMeta);
   }
 
   return {
@@ -116,18 +139,18 @@ async function parserAuthorizedDownloadFile(songWebUrl) {
     req: {
       url: downUrl,
     },
-  }
+  };
 }
 
-async function parserDownloadUrl(songWebUrl) {
+async function parserDownloadUrl(songWebUrl, settings) {
   // Anonymous
-  let url = ""
-  if (gopeed.settings.cookie == "") {
-    url = await parserAnonymousDownloadUrl(songWebUrl)
-  
-  // Authorized with Cookie
+  let url = "";
+  if (settings.cookie == "") {
+    url = await parserAnonymousDownloadUrl(songWebUrl);
+
+    // Authorized with Cookie
   } else {
-    url = await parserAuthorizedDownloadUrl(songWebUrl)
+    url = await parserAuthorizedDownloadUrl(songWebUrl, settings);
   }
   return url;
 }
@@ -136,51 +159,21 @@ async function parserAnonymousDownloadUrl(songWebUrl) {
   return libsong.AnonymousWebUrlToPlayUrl(songWebUrl);
 }
 
-async function parserAuthorizedDownloadUrl(songWebUrl) {
+async function parserAuthorizedDownloadUrl(songWebUrl, settings) {
+  const html = await libsong.FetchSongWebHtmlAnonymous(songWebUrl, settings);
 
-  const html = await libsong.FetchSongWebHtmlAnonymous(songWebUrl, gopeed.settings);
+  const songId = libsong.ParserSongId(html);
 
-  const songId = libsong.ParserSongId(html)
-
-  const downMeta = await libsong.FetchSongDownMetaJson(songId, gopeed.settings)
+  const downMeta = await libsong.FetchSongDownMetaJson(songId, settings);
 
   // 当歌曲不提供下载时下载试听版本
   if (!downMeta["ok"]) {
-    return parserAnonymousDownloadFile(ctx);
+    return parserAnonymousDownloadUrl(ctx);
   }
 
-  let downUrl = libsong.GetLowMP3UrlFromMetaJson(downMeta)
+  let downUrl = libsong.GetLowMP3UrlFromMetaJson(downMeta);
   if (gopeed.settings.quality == "songhigh") {
-    downUrl = libsong.GetHighMP3UrlFromMetaJson(downMeta)
+    downUrl = libsong.GetHighMP3UrlFromMetaJson(downMeta);
   }
-  return downUrl
-}
-
-// 专辑
-async function handleAlbum(ctx) {
-
-  const albumWebUrl = ctx.req.url;
-
-  const albumWebHtml = await libalbum.FetchAlbumWebHtmlAnonymous(albumWebUrl, gopeed.settings)
-
-  const albumList = libalbum.ParserSongWebUrlList(albumWebHtml)
-
-  let files = []
-  for (var i = 0; i < albumList.length; i++) {
-
-    let songItem = albumList[i]
-
-    let name = songItem["name"] + ".mp3"
-    let url = await parserDownloadUrl(songItem["path"])
-    // throw new MessageError(url);
-
-    let file = {
-      name: name,
-      req: {
-        url: url,
-      },
-    }
-    files.push(file);
-  }
-  return files
+  return downUrl;
 }
--- src/lib/album.js
+++ src/lib/album.js
@@ -1,41 +1,37 @@
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
 // 获取音乐 HTML 页面内容
 export async function FetchAlbumWebHtmlAnonymous(albumWebUrl, settings) {
-
-    const resp = await fetch(albumWebUrl, {
-      headers: {
-        'User-Agent': settings.ua,
-      },
-    });
-    return await resp.text();
+  const resp = await fetch(albumWebUrl, {
+    headers: {
+      "User-Agent": settings.ua,
+    },
+  });
+  return await resp.text();
 }
 
 // 从 HTML 页面中解析出歌曲列表
 export function ParserSongWebUrlList(albumWebHtml) {
+  // 使用 cheerio 加载网页内容
+  const $ = cheerio.load(albumWebHtml);
 
-    // 使用 cheerio 加载网页内容
-    const $ = cheerio.load(albumWebHtml);
-  
-    // 获取 <div class="songs mt5"> 内的 <table> 下所有 <tr> 元素
-    const trElements = $('.songs.mt5 table tr');
+  // 获取 <div class="songs mt5"> 内的 <table> 下所有 <tr> 元素
+  const trElements = $(".songs.mt5 table tr");
 
-    // 遍历并输出每个<tr>元素
-    let list = []
-    trElements.each((i, element) => {
+  // 遍历并输出每个<tr>元素
+  let list = [];
+  trElements.each((i, element) => {
+    let name = $(element).find("td.name").text().trim();
+    // console.log(`TD.name 元素 ${i}:`, name);
 
-        let name = $(element).find('td.name').text().trim();
-        // console.log(`TD.name 元素 ${i}:`, name);
+    let href = $(element).find("td.name a").attr("href").trim();
+    //   console.log(`TD.name href 元素 ${i}:`, href);
 
-        let href = $(element).find('td.name a').attr("href").trim();
-        //   console.log(`TD.name href 元素 ${i}:`, href);
-
-        let item = {
-            "name": name,
-            "path": "https://www.zanmei.ai" + href
-        }
-        list.push(item);
-    });
-    return list;
+    let item = {
+      name: name,
+      path: "https://www.zanmei.ai" + href,
+    };
+    list.push(item);
+  });
+  return list;
 }
-
